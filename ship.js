d4a53a4d109d2edf3a8c9c9cecb5f2ae52ff5ecc
Vadim Kostrov <vmkvo15@outlook.com>
2023-07-17 14:46:15 +0400

    Add length attribute and error handling

--- ship.js
+++ ship.js
@@ -1,9 +1,15 @@
-const Ship = () => ({
-  length: 5,
-  hits: 0,
-  hit() {
-    if (this.hits < this.length) { this.hits += 1; }
-  },
-});
+const Ship = (length) => {
+  if (length < 1 || length > 5) {
+    return 'Length Error';
+  }
+
+  return {
+    length,
+    hits: 0,
+    hit() {
+      if (this.hits < this.length) { this.hits += 1; }
+    },
+  };
+};
 
 module.exports = Ship;
--- ship.test.js
+++ ship.test.js
@@ -2,9 +2,23 @@
 const Ship = require('./ship');
 
 describe('Ship Constructor Tests', () => {
-  const ship = Ship();
+  const ship = Ship(5);
   test('Return object from Ship factory function', () => {
     expect(typeof ship).toBe('object');
+  });
+
+  test('Return ship with given length attribute', () => {
+    const ship1 = Ship(5);
+    const ship2 = Ship(4);
+    const ship3 = Ship(2);
+    expect(ship1.length).toBe(5);
+    expect(ship2.length).toBe(4);
+    expect(ship3.length).not.toEqual(-10);
+  });
+
+  test('Return error if given length exceeds 5 or is less than 1', () => {
+    const shipError = Ship(-10);
+    expect(shipError).toMatch('Length Error');
   });
 
   describe('Ship Hit Mechanics', () => {
