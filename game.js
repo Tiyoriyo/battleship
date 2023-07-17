9f2a07ff4e70a02bd9c6a446adf3110db92ed36f
Vadim Kostrov <vmkvo15@outlook.com>
2023-07-17 22:11:31 +0400

    Refactor code and add multidirection functionality to placeShip

--- game.js
+++ game.js
@@ -3,19 +3,32 @@ const Game = () => {
     const board = [];
     for (let i = 0; i < 10; i += 1) {
       const row = [];
-      for (let j = 0; j < 10; j += 1) {
-        row[j] = 'empty';
-      }
-      board[i] = row;
+      for (let j = 0; j < 10; j += 1) { row[j] = 'empty'; }
+      board.push(row);
     }
     return board;
   }
 
   function checkTrack(ship, x, y, direction, board) {
-    const curX = x; let curY = y;
+    let curX = x; let curY = y;
     for (let i = 0; i < ship.length; i += 1) {
       if (curX > 9 || curX < 0 || curY > 9 || curY < 0) return false;
-      if (direction === 'down') { curY += 1; }
+      switch (direction) {
+        case 'down':
+          curY += 1;
+          break;
+        case 'up':
+          curY -= 1;
+          break;
+        case 'left':
+          curX -= 1;
+          break;
+        case 'right':
+          curX += 1;
+          break;
+        default:
+          break;
+      }
     }
     return board[curY][curX];
   }
@@ -23,11 +36,26 @@ const Game = () => {
   return {
     board: createBoard(),
     placeShip(ship, x, y, direction) {
-      const curX = x; let curY = y;
+      let curX = x; let curY = y;
       if (!checkTrack(ship, x, y, direction, this.board)) return 'Error';
       for (let i = 0; i < ship.length; i += 1) {
         this.board[curY][curX] = 'ship';
-        if (direction === 'down') { curY += 1; }
+        switch (direction) {
+          case 'down':
+            curY += 1;
+            break;
+          case 'up':
+            curY -= 1;
+            break;
+          case 'left':
+            curX -= 1;
+            break;
+          case 'right':
+            curX += 1;
+            break;
+          default:
+            break;
+        }
       }
     },
   };
--- gameboard.test.js
+++ gameboard.test.js
@@ -22,4 +22,20 @@ describe('Gameboard Tests', () => {
     const ship2 = Ship(5);
     expect(game.placeShip(ship2, 4, 8, 'down')).toBe('Error');
   });
+
+  test(('All Directions have placeShip functionality'), () => {
+    const ship3 = Ship(4);
+    const ship4 = Ship(4);
+    const ship5 = Ship(4);
+    const ship6 = Ship(4);
+
+    expect(game.placeShip(ship3, 8, 9, 'right')).toBe('Error');
+    expect(game.placeShip(ship3, 1, 8, 'right')).not.toBe('Error');
+    expect(game.placeShip(ship4, 1, 2, 'left')).toBe('Error');
+    expect(game.placeShip(ship4, 4, 0, 'left')).not.toBe('Error');
+    expect(game.placeShip(ship5, 4, 1, 'up')).toBe('Error');
+    expect(game.placeShip(ship5, 6, 5, 'up')).not.toBe('Error');
+    expect(game.placeShip(ship6, 4, 7, 'down')).toBe('Error');
+    expect(game.placeShip(ship6, 7, 4, 'down')).not.toBe('Error');
+  });
 });
