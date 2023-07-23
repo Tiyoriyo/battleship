e28309cbd08eb9d378f2a448f571ae6efc8245bf
Vadim Kostrov <vmkvo15@outlook.com>
2023-07-23 08:17:27 +0400

    Refactor ship tests

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -7,53 +7,58 @@ const hitDebug = (ship, num) => {
 };
 
 describe('Ship Constructor Tests', () => {
-  test('Return object from Ship factory function', () => {
-    const ship1 = Ship(5);
-    expect(typeof ship1).toBe('object');
+  let ship1 = Ship(5);
+  let ship2 = Ship(4);
+  let ship3 = Ship(2);
+
+  beforeEach(() => {
+    ship1 = Ship(5);
+    ship2 = Ship(4);
+    ship3 = Ship(2);
   });
 
-  test('Return ship with given length attribute', () => {
-    const ship1 = Ship(5);
-    const ship2 = Ship(4);
-    const ship3 = Ship(2);
-    expect(ship1.length).toBe(5);
-    expect(ship2.length).toBe(4);
-    expect(ship3.length).not.toEqual(-10);
-  });
+  describe('Ship Creation Test', () => {
+    test('Return object from Ship factory function', () => {
+      expect(typeof ship1).toBe('object');
+    });
 
-  test('Return error if given length exceeds 5 or is less than 1', () => {
-    const ship = Ship(-10);
-    expect(ship).toMatch('Length Error');
+    test('Return ship with given length attribute', () => {
+      expect(ship1.length).toBe(5);
+      expect(ship2.length).toBe(4);
+      expect(ship3.length).not.toEqual(-10);
+    });
+
+    test('Return error if given length exceeds 5 or is less than 1', () => {
+      const ship1 = Ship(-10);
+      expect(ship1).toMatch('Length Error');
+    });
   });
 
   describe('Ship Hit Mechanics', () => {
     test('Hit Count increases when ship is hit', () => {
-      const ship = Ship(5);
-      ship.damage();
-      expect(ship.hits).toBe(1);
+      const ship1 = Ship(5);
+      ship1.damage();
+      expect(ship1.hits).toBe(1);
     });
 
     test('Battlefield sunk becomes true when the hit count reaches the length', () => {
-      const ship = Ship(5);
-      hitDebug(ship, 5);
-      expect(ship.sunk).toBeTruthy();
+      const ship1 = Ship(5);
+      hitDebug(ship1, 5);
+      expect(ship1.sunk).toBeTruthy();
     });
 
     test('Ship indicates it has been sunk when length = hits', () => {
-      const ship = Ship(5);
-      expect(ship.isSunk()).toBe(false);
+      const ship1 = Ship(5);
+      expect(ship1.isSunk()).toBe(false);
 
-      hitDebug(ship, 2);
-      expect(ship.isSunk()).toBe(false);
-
-      hitDebug(ship, 13);
-      expect(ship.isSunk()).toBe(true);
+      hitDebug(ship1, 13);
+      expect(ship1.isSunk()).toBe(true);
     });
 
     test('Hit Count does not increase past the length of the ship', () => {
-      const ship = Ship(5);
-      hitDebug(ship, 10);
-      expect(ship.hits).toBe(5);
+      const ship1 = Ship(5);
+      hitDebug(ship1, 10);
+      expect(ship1.hits).toBe(5);
     });
   });
 });
