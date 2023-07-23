aba2704c3caaea8cd91eec046ccfb94842d35af5
Vadim Kostrov <vmkvo15@outlook.com>
2023-07-23 09:47:39 +0400

    Add player constructor and include methods to track active ships

--- modules/game.js
+++ modules/game.js
@@ -1,4 +1,7 @@
 /* eslint-disable consistent-return */
+
+import Player from './player';
+
 /* eslint-disable no-unused-vars */
 const Game = () => {
   function createBoard() {
@@ -14,6 +17,23 @@ const Game = () => {
       board.push(column);
     }
     return board;
+  }
+
+  function getShipType(num) {
+    switch (num) {
+      case 5: return 'carrier';
+      case 4: return 'battleship';
+      case 3: return 'destroyer';
+      case 2: return 'patrol';
+      default: break;
+    }
+  }
+
+  function checkShipAvailability(player, num) {
+    const shipType = getShipType(num);
+    const index = player.shipArsenal.indexOf(shipType);
+    if (index > -1) { return true; }
+    return false;
   }
 
   function checkTrack(ship, x, y, direction, board) {
@@ -32,6 +52,28 @@ const Game = () => {
     return true;
   }
 
+  function activateShip(player, ship) {
+    const index = player.shipArsenal.indexOf(getShipType(ship.length));
+    player.activeShips.push(ship);
+  }
+
+  function placeShip(ship, x, y, direction) {
+    let curX = x; let curY = y;
+    if (!checkTrack(ship, x, y, direction, this.board)) return 'Error';
+    if (!checkShipAvailability(this.player, ship.length)) return 'Error: Ship is not available';
+    activateShip(this.player, ship);
+    for (let i = 0; i < ship.length; i += 1) {
+      this.board[curX][curY].ship = ship;
+      switch (direction) {
+        case 'down': curY += 1; break;
+        case 'up': curY -= 1; break;
+        case 'left': curX -= 1; break;
+        case 'right': curX += 1; break;
+        default: break;
+      }
+    }
+  }
+
   function attack(x, y) {
     if (x > 9 || x < 0 || y > 9 || y < 0) { return 'Error: Attack is not within bounds'; }
     const square = this.board[x][y];
@@ -47,20 +89,9 @@ const Game = () => {
 
   return {
     board: createBoard(),
-    placeShip(ship, x, y, direction) {
-      let curX = x; let curY = y;
-      if (!checkTrack(ship, x, y, direction, this.board)) return 'Error';
-      for (let i = 0; i < ship.length; i += 1) {
-        this.board[curX][curY].ship = ship;
-        switch (direction) {
-          case 'down': curY += 1; break;
-          case 'up': curY -= 1; break;
-          case 'left': curX -= 1; break;
-          case 'right': curX += 1; break;
-          default: break;
-        }
-      }
-    },
+    player: Player(),
+    computer: undefined,
+    placeShip,
     attack,
   };
 };
---
+++ modules/player.js
@@ -0,0 +1,8 @@
+const Player = () => ({
+  activeShips: [],
+  sunkShips: [],
+  shipArsenal: ['carrier', 'battleship', 'battleship',
+    'destroyer', 'destroyer', 'destroyer', 'patrol', 'patrol', 'patrol', 'patrol'],
+});
+
+export default Player;
