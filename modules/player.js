d485078adf45e5856abc3ff81a124b0f3d1542ce
Vadim Kostrov <vmkvo15@outlook.com>
2023-07-24 18:19:29 +0400

    Add createBoard function

--- modules/player.js
+++ modules/player.js
@@ -1,9 +1,26 @@
-const Player = (name) => ({
-  name,
-  activeShips: [],
-  sunkShips: [],
-  shipArsenal: ['carrier', 'battleship', 'battleship',
-    'destroyer', 'destroyer', 'destroyer', 'patrol', 'patrol', 'patrol', 'patrol'],
-});
+const Player = (name) => {
+  function createBoard() {
+    const board = [];
+    for (let i = 0; i < 10; i += 1) {
+      const column = [];
+      for (let j = 0; j < 10; j += 1) {
+        column[j] = {
+          ship: undefined,
+          status: null,
+        };
+      }
+      board.push(column);
+    }
+    return board;
+  }
+  return {
+    name,
+    board: createBoard(),
+    activeShips: [],
+    sunkShips: [],
+    shipArsenal: ['carrier', 'battleship', 'battleship',
+      'destroyer', 'destroyer', 'destroyer', 'patrol', 'patrol', 'patrol', 'patrol'],
+  };
+};
 
 export default Player;
