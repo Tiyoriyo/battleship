10013c252b7a8a3b50db3ebbff7165de9d3cc5af
Vadim Kostrov <vmkvo15@outlook.com>
2023-07-24 16:49:50 +0400

    Refactor code to correspond to two players

--- modules/game.js
+++ modules/game.js
@@ -1,6 +1,8 @@
+/* eslint-disable import/extensions */
 /* eslint-disable consistent-return */
 
-import Player from './player';
+import Player from './player.js';
+import Ship from './ship.js';
 
 /* eslint-disable no-unused-vars */
 const Game = () => {
@@ -28,6 +30,16 @@ const Game = () => {
       default: break;
     }
   }
+
+  // function getShipLength(string) {
+  //   switch (string) {
+  //     case 'carrier': return 5;
+  //     case 'battleship': return 4;
+  //     case 'destroyer': return 3;
+  //     case 'patrol': return 2;
+  //     default: break;
+  //   }
+  // }
 
   function checkShipAvailability(player, num) {
     const shipType = getShipType(num);
@@ -58,12 +70,13 @@ const Game = () => {
     player.activeShips.push(ship);
   }
 
-  function placeShip(ship, x, y, direction) {
+  function placeShip(player, ship, x, y, direction) {
     let curX = x; let curY = y;
-    if (!checkTrack(ship, x, y, direction, this.board)) return 'Error';
-    if (!checkShipAvailability(this.player, ship.length)) return 'Error: Ship is not available';
+    const board = (player.name === 'player') ? this.playerBoard : this.enemyBoard;
+    if (!checkTrack(ship, x, y, direction, board)) return 'Error';
+    if (!checkShipAvailability(player, ship.length)) return 'Error: Ship is not available';
     for (let i = 0; i < ship.length; i += 1) {
-      this.board[curX][curY].ship = ship;
+      board[curX][curY].ship = ship;
       switch (direction) {
         case 'down': curY += 1; break;
         case 'up': curY -= 1; break;
@@ -71,7 +84,7 @@ const Game = () => {
         case 'right': curX += 1; break;
         default: break;
       }
-    } activateShip(this.player, ship);
+    } activateShip(player, ship);
     return true;
   }
 
@@ -83,26 +96,40 @@ const Game = () => {
     }
   }
 
-  function attack(x, y) {
+  function attack(player, board, x, y) {
     if (x > 9 || x < 0 || y > 9 || y < 0) { return 'Error: Attack is not within bounds'; }
-    const square = this.board[x][y];
+    const square = board[x][y];
     if (square.status) { return 'Error: Already Attacked'; }
     if (square.ship) {
       square.status = 'hit';
       square.ship.damage();
-      checkSunkStatus(this.player, square.ship);
+      checkSunkStatus(player, square.ship);
       return true;
     }
     square.status = 'miss';
     return false;
   }
 
+  // function computerSetup() {
+  //   for (let i = 0; i < this.computer.shipArsenal.length; i += 1) {
+  //     while (this.computer.activeShips.length < this.computer.activeShips.length + 1) {
+  //       const x = Math.floor(Math.random() * 10);
+  //       const y = Math.floor(Math.random() * 10);
+  //       const directionList = ['left', 'right', 'up', 'down'];
+  //       const direction = directionList[Math.floor(Math.random() * 4)];
+  //       placeShip(Ship(getShipLength(this.computer.shipArsenal[i])), x, y, direction, this.board, this.player);
+  //     }
+  //   }
+  // }
+
   return {
-    board: createBoard(),
-    player: Player(),
-    computer: undefined,
+    playerBoard: createBoard(),
+    enemyBoard: createBoard(),
+    player: Player('player'),
+    computer: Player('computer'),
     placeShip,
     attack,
+    // computerSetup,
   };
 };
 
