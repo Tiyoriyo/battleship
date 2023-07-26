c8a3d457d287c122ad9580ee5a390e8f89c3c3ee
Vadim Kostrov <vmkvo15@outlook.com>
2023-07-26 13:49:57 +0400

    Add computer setup function and getFreeSquares func

--- modules/game.js
+++ modules/game.js
@@ -16,15 +16,15 @@ const Game = () => {
     }
   }
 
-  // function getShipLength(string) {
-  //   switch (string) {
-  //     case 'carrier': return 5;
-  //     case 'battleship': return 4;
-  //     case 'destroyer': return 3;
-  //     case 'patrol': return 2;
-  //     default: break;
-  //   }
-  // }
+  function getShipLength(string) {
+    switch (string) {
+      case 'carrier': return 5;
+      case 'battleship': return 4;
+      case 'destroyer': return 3;
+      case 'patrol': return 2;
+      default: break;
+    }
+  }
 
   function checkShipAvailability(player, num) {
     const shipType = getShipType(num);
@@ -102,24 +102,38 @@ const Game = () => {
     return false;
   }
 
-  // function computerSetup() {
-  //   for (let i = 0; i < this.computer.shipArsenal.length; i += 1) {
-  //     while (this.computer.activeShips.length < this.computer.activeShips.length + 1) {
-  //       const x = Math.floor(Math.random() * 10);
-  //       const y = Math.floor(Math.random() * 10);
-  //       const directionList = ['left', 'right', 'up', 'down'];
-  //       const direction = directionList[Math.floor(Math.random() * 4)];
-  //       placeShip(Ship(getShipLength(this.computer.shipArsenal[i])), x, y, direction, this.board, this.player);
-  //     }
-  //   }
-  // }
+  function computerSetup() {
+    const { computer } = this;
+    const directionList = ['left', 'right', 'up', 'down'];
+    for (let i = 0; i < 10; i += 1) {
+      while (computer.activeShips.length < computer.activeShips.length + 1) {
+        if (computer.activeShips.length === 10) return;
+        const x = Math.floor(Math.random() * 10);
+        const y = Math.floor(Math.random() * 10);
+        const direction = directionList[Math.floor(Math.random() * 4)];
+        const shipLength = getShipLength(computer.shipArsenal[i]);
+        placeShip(computer, Ship(shipLength), x, y, direction);
+      }
+    }
+  }
+
+  function getFreeSquares(player) {
+    let total = 0;
+    const { board } = player;
+    for (let i = 0; i < board.length; i += 1) {
+      for (let j = 0; j < board[i].length; j += 1) {
+        if (!board[i][j].status) total += 1;
+      }
+    }
+  }
 
   return {
     player: Player('player'),
     computer: Player('computer'),
     placeShip,
     attack,
-    // computerSetup,
+    computerSetup,
+    getFreeSquares,
   };
 };
 
