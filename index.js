5f58344840cc25e95f51ffcb57d033330e5d2b18
Vadim Kostrov <vmkvo15@outlook.com>
2023-07-31 22:13:17 +0400

    Add DOM board setup func

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+/* eslint-disable import/extensions */
+import Game from './modules/game.js';
+
+const game = Game();
 const mainboard = document.querySelector('.mainboard');
 
-for (let i = 0; i < 10; i += 1) {
+for (let i = 0; i < 10; i++) {
   const column = document.createElement('div');
   column.classList.add('column');
-  for (let j = 0; j < 10; j += 1) {
+  for (let j = 0; j < 10; j++) {
     const square = document.createElement('div');
+    const content = document.createElement('div');
     square.classList.add('square');
-    square.textContent = `${i}, ${j}`;
-    column.append(square);
+    content.classList.add('content');
+
+    square.appendChild(content);
+    column.appendChild(square);
   }
   mainboard.appendChild(column);
 }
+
+// console.log([...mainboard.childNodes][1]);
+console.log(mainboard.childNodes[0].childNodes[1].childNodes[0].innerHTML = '&#183;');
