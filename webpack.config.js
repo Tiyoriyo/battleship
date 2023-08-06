77759355056b83b9f1fa5f4ef63806db45330709
Vadim Kostrov <vmkvo15@outlook.com>
2023-08-06 12:54:49 +0400

    Dev Setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
